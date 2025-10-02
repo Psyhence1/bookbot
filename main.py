faf843dd050f1f7b73771569983c5314bb89f323
psyhence1 <marco.a.lopez.m@gmail.com>
2025-10-02 02:17:18 -0600

    finished project ver 1.0

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import sys
 from stats import get_word_count, get_char_dict, sorted_char_dict
 
-
+# main function to run the program
 def main():
-    book_path  = "books/frankenstein.txt"
+    book_path = get_book_path()    
     book_text = get_book_text(book_path)
     num_words = get_word_count(book_text)
     char_dict = get_char_dict(book_text)
     sorted_dict = sorted_char_dict(char_dict)
     print_report(book_path, num_words, sorted_dict)
 
+# retrieves path to book from command line arguments
+def get_book_path():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return sys.argv[1]
+
+# reads the book file and returns its text
 def get_book_text(book_path):
     with open(book_path) as ebook:
         return ebook.read()
-
+    
+# prints the analysis report
 def print_report(book_path, num_words, sorted_dict):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
@@ -26,5 +36,6 @@ def print_report(book_path, num_words, sorted_dict):
 
     print("============== END ==============")
 
-    
+# prints list of strings passed to python3 via line arguments
+    print(sys.argv)
 main()
